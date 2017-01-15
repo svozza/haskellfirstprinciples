48fc10a53dbf833b4ac231e9b22e6d68f6a0adbd
Stefano Vozza <svozza@gmail.com>
2017-01-15 23:27:27 +0000

    fix dividedBy

--- chapter08/exercise02.hs
+++ chapter08/exercise02.hs
@@ -8,10 +8,20 @@ mult' multiplicand multiplier = go (abs multiplicand) multiplier 0
          | md == 0   = if multiplicand < 0 then (negate result) else result
          | otherwise = go (md - 1) m (result + m)
 
-dividedBy :: Integral a => a -> a -> (a, a)
-dividedBy num denom = go num denom 0
+data DividedResult a = Result a | DividedByZero
+  deriving (Show, Eq)
+
+dividedBy' :: Integral a => a -> a -> (a, a)
+dividedBy' num denom  = go (abs num) (abs denom) 0
   where go n d count
-         | num > 0 && n < d = (count, n)
-         | num < 0 && n > d = (count, n)
-         | num < 0 && n < d = go (n + d) d (count + 1)
-         | otherwise        = go (n - d) d (count + 1)
+         | n < d     = if isOneNegative num denom then ((negate count), n) else (count, n)
+         | otherwise = go (n - d) d (count + 1)
+        isOneNegative a b
+         | a < 0 && b > 0 = True
+         | a > 0 && b < 0 = True
+         | otherwise      = False
+
+dividedBy :: Integer -> Integer -> DividedResult Integer
+dividedBy _ 0 = DividedByZero
+dividedBy num denom = case (dividedBy' num denom) of
+                           (x, y) -> Result x
