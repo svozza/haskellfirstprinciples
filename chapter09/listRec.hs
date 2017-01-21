13a43a8759c35fc4a66be628bcf2320235cdec89
Stefano Vozza <svozza@gmail.com>
2017-01-21 18:40:15 +0000

    handle case where n is higher than list size in splitAt

--- chapter09/listRec.hs
+++ chapter09/listRec.hs
@@ -8,11 +8,11 @@ drop' _ []     = []
 drop' 0 xs     = xs
 drop' n (x:xs) = drop' (n - 1) xs
 
-splitAt'' :: Int -> ([a], [a]) -> ([a], [a])
-splitAt'' _ (xs, []) = (xs, [])
-splitAt'' 0 (xs, ys) = (reverse xs, ys)
-splitAt'' n (xs, y:ys) = splitAt'' (n - 1) (y:xs, ys)
-
 splitAt' :: Int -> [a] -> ([a], [a])
-splitAt' n xs = splitAt'' n ([], xs)
-
+splitAt' num xs
+  | num > length xs = (xs, [])
+  | otherwise       = go num ([], xs)
+  where
+    go n (ys, []) = (ys, [])
+    go 0 (ys, zs) = (reverse ys, zs)
+    go n (ys, z:zs) = go (n - 1) (z:ys, zs)
