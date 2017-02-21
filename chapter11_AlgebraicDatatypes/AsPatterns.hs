302850965351ffd93860f6064e3f69c361ea46db
Stefano Vozza <svozza@gmail.com>
2017-02-21 00:37:54 +0000

    add AsPattern languange exercises

--- chapter11_AlgebraicDatatypes/AsPatterns.hs
+++ chapter11_AlgebraicDatatypes/AsPatterns.hs
@@ -1,13 +1,30 @@
 import Data.Char
+import Data.List
 
 isSubsequenceOf' :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf' sub ys = sub == (fst $ tuple ys)
-  where tuple = foldr (\x (s, y:ys) ->  
+  where tuple = foldr (\x (z, ys@(y:ys')) ->  
                   if x == y
-                  then (x:s, ys)
-                  else (s, y:ys)) 
+                  then (x:z, ys')
+                  else (z, ys)) 
                 ([], (reverse sub))
 
 capitalizeWords :: String -> [(String, String)]
 capitalizeWords = map capitalize . words 
   where capitalize s@(x:xs) = (s, (toUpper x):xs)
+
+capitalizeWord :: String -> String
+capitalizeWord (s:ss) = (toUpper s):ss
+
+splitOn :: Char -> String -> [String]
+splitOn c s = go c (reverse s) []
+  where go _ [] xs    = xs
+        go c (s:ss) []
+          | c == s    = go c ss ([]:[])
+          | otherwise = go c ss ([s]:[])
+        go c (s:ss) (xs:xss)
+          | c == s    = go c ss ([]:xs:xss)
+          | otherwise = go c ss ((s:xs):xss)
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph = intercalate ". " . map (capitalizeWord . dropWhile isSpace) . splitOn '.'
